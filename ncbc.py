3022563c2965dc3f1106a601f3d839ba12b3d58f
Kevin Thorngren <kthorngr@cisco.com>
2018-09-20 09:15:08 -0400

    Added prompt to email labels to brewers

--- ncbc.py
+++ ncbc.py
@@ -1512,7 +1512,16 @@ def process_new_entries(pkid=1):
                 result = False
             else:
                 result = False
-                #result = e.send_message(message)
+                try:
+                    choice = int(input('Do you wish to email the brewers? '))
+                except ValueError as e:
+                    choice = 'n'
+
+                if choice.lower() == 'y':
+                    result = e.send_message(message)
+                else:
+                    logger.info('Skipping emailing brewers - please run script again to email')
+                    result = False
 
             if result:
                 n.reset_send_labels(brewer)
