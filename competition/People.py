31eecbeadc8a0ff670fd4861e1aa29a6ea303024
Kevin Thorngren <kevin.thorngren@gmail.com>
2019-05-03 20:45:46 -0400

    Updated comments

--- competition/People.py
+++ competition/People.py
@@ -16,14 +16,38 @@ class People:
         pass
 
     def _get_fields(self, fields):
+        """Returns a str of comma separated fields.
 
+        Args:
+            fields (list): List of fields to retrieve.
+
+        Returns:
+            str: Comma separated fields in a str.
+        """
         default_fields = ['pkid', 'firstname', 'lastname', 'nickname', 'email', 'alias']
 
         return ",".join(fields) if fields else ",".join(default_fields)
 
     def get_all_people(self, pkid=None, name=None, firstname=None, lastname=None,
                    nickname=None, email=None, alias=None, fields=None, filter=None):
+        """Fetches people from the DB.
 
+        Builds SQL query, based on parameters, to fetch one or more people.  The filters are `and` based.
+
+        Args:
+            pkid (int): PKID of individual person to fetch.
+            name: (str): Uses like wild card for firstname, lastname or nickname matches.
+            firstname (str): Find where firstname =.
+            lastname (str): Find where lastname =.
+            nickname (str): Find where nickname =.
+            email (str): Find where email =.
+            alias (int): alias is PKID pointer for alias record to point to main record in table.
+            fields (list): List of fields to retrieve for each person.
+            filters (str | list): Custom filters to use in where clause.
+
+        Returns:
+            (dict | list of dicts):  Returns dict if one person (pkid) else list of dicts.
+        """
         fields = self._get_fields(fields)
         sql = f'select {fields} from people'
 
@@ -63,37 +87,50 @@ class People:
         return result
 
     def get_person(self, pkid=None, firstname=None, lastname=None, nickname=None, email=None, alias=None, fields=None):
+        """Returns person's main record plus alias info combined.
 
+
+        """
         result = self.get_all_people(pkid=pkid, firstname=firstname, lastname=lastname,
                                  nickname=nickname, email=email, alias=alias, fields=fields)
 
-        if len(result) == 1:
+        # Return the result is list of one element or dict (pkid search)
+        if len(result) == 1 and isinstance(result, list):
             return result[0]
+        elif isinstance(result, dict):
+            return result
 
         people = {}
         alias = []
+
+        # Get a list of (main.pkid, alias.pkid) pairs.
         for person in result:
             people[person['pkid']] = person
             if person['alias']:
                 alias.append((person['pkid'], person['alias']))
 
+        # If more than one alias, won't be able to determine specifc person.
         if len(alias) > 1:
             logger.error(f'Error too many aliases: {alias}')
             for r in result:
                 logger.debug(r)
             return None
 
+
         if alias:
-            alias = alias[0]
+            alias = alias[0]  # Get the single alias list element
 
             alias_record = people[alias[1]]
             main_record = people[alias[0]]
 
+            # Alias info is preferred over main record info.
             for k, v in alias_record.items():
                 if v:
                     main_record[k] = v
 
             return main_record
+
+        # Get list of all PKID's returned for debugging purposes.
         x = [x['pkid'] for x in result]
         logger.error(f'Too many records to determine person: pkids: {x}')
 
@@ -105,7 +142,21 @@ class People:
 
 
     def get_aliases(self, pkid=None, firstname=None, lastname=None, nickname=None, email=None, alias=None, fields=None):
+        """Get records that are aliases.
 
+        Args:
+            pkid (int): PKID of individual person to fetch.
+            name: (str): Uses like wild card for firstname, lastname or nickname matches.
+            firstname (str): Find where firstname =.
+            lastname (str): Find where lastname =.
+            nickname (str): Find where nickname =.
+            email (str): Find where email =.
+            alias (int): alias is PKID pointer for alias record to point to main record in table.
+            fields (list): List of fields to retrieve for each person.
+
+        Returns:
+            (dict | list of dicts):  Returns dict if one person (pkid) else list of dicts.
+        """
         result = self.get_all_people(pkid=pkid, firstname=firstname, lastname=lastname,
                                  nickname=nickname, email=email, alias=alias, fields=fields,
                                  filter='alias>0'
@@ -115,7 +166,22 @@ class People:
 
 
     def get_non_aliases(self, pkid=None, firstname=None, lastname=None, nickname=None, email=None, alias=None, fields=None):
+        """Returns records that are not aliases.
 
+        Args:
+            pkid (int): PKID of individual person to fetch.
+            name: (str): Uses like wild card for firstname, lastname or nickname matches.
+            firstname (str): Find where firstname =.
+            lastname (str): Find where lastname =.
+            nickname (str): Find where nickname =.
+            email (str): Find where email =.
+            alias (int): alias is PKID pointer for alias record to point to main record in table.
+            fields (list): List of fields to retrieve for each person.
+
+        Returns:
+            (dict | list of dicts):  Returns dict if one person (pkid) else list of dicts.
+
+        """
         result = self.get_all_people(pkid=pkid, firstname=firstname, lastname=lastname,
                                  nickname=nickname, email=email, alias=alias, fields=fields,
                                  filter='alias=0'
@@ -125,7 +191,21 @@ class People:
 
 
     def get_people(self, pkid=None, firstname=None, lastname=None, nickname=None, email=None, alias=None, fields=None):
+        """Returns one or more poeple combined with alias information.
 
+        Args:
+            pkid (int): PKID of individual person to fetch.
+            name: (str): Uses like wild card for firstname, lastname or nickname matches.
+            firstname (str): Find where firstname =.
+            lastname (str): Find where lastname =.
+            nickname (str): Find where nickname =.
+            email (str): Find where email =.
+            alias (int): alias is PKID pointer for alias record to point to main record in table.
+            fields (list): List of fields to retrieve for each person.
+
+        Returns:
+            (dict | list of dicts):  Returns dict if one person (pkid) else list of dicts.
+        """
         aliases = self.get_aliases()
 
         alias_map = {}
